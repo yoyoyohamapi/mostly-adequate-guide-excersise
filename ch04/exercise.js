b086cbf836239e481e70296af007a1cecd309759
surejinwu(吴晓军) <surejinwu@tencent.com>
2017-03-25 15:36:23 +0800

    ch4 completed

--- ch04/exercise.js
+++ ch04/exercise.js
@@ -1,32 +1,35 @@
-[mostly adequate guide](https://www.gitbook.com/book/drboolean/mostly-adequate-guide)练习题
-==============
 var _ = require('ramda');
+
 
 // Exercise 1
 //==============
 // Refactor to remove all arguments by partially applying the function.
 
-var words = function(str) {
-  return _.split(' ', str);
-};
+words = _.split(' ');
+
 
 // Exercise 1a
 //==============
 // Use map to make a new words fn that works on an array of strings.
 
-var sentences = undefined;
+var sentences = _.map(words);
 
 
 // Exercise 2
 //==============
 // Refactor to remove all arguments by partially applying the functions.
 
-var filterQs = function(xs) {
-  return _.filter(function(x) {
-    return match(/q/i, x);
-  }, xs);
-};
+// var filterQs = function (xs) {
+//     return _.filter(function (x) {
+//         return match(/q/i, x);
+//     }, xs);
+// };
 
+var match = _.curry(function (regex, str) {
+    return str.match(regex);
+});
+
+var filterQs = _.filter(match(/q/i));
 
 // Exercise 3
 //==============
@@ -34,27 +37,33 @@ var filterQs = function(xs) {
 // arguments.
 
 // LEAVE BE:
-var _keepHighest = function(x, y) {
-  return x >= y ? x : y;
+var _keepHighest = function (x, y) {
+    return x >= y ? x : y;
 };
 
 // REFACTOR THIS ONE:
-var max = function(xs) {
-  return _.reduce(function(acc, x) {
-    return _keepHighest(acc, x);
-  }, -Infinity, xs);
-};
-
+var max = _.reduce(_keepHighest, -Infinity);
 
 // Bonus 1:
 // ============
 // Wrap array's slice to be functional and curried.
 // //[1, 2, 3].slice(0, 2)
-var slice = undefined;
+var slice = _.curry(function(start, end, arr) {
+    return arr.slice(start, end);
+});
 
 
 // Bonus 2:
 // ============
 // Use slice to define a function "take" that takes n elements from the beginning of the string. Make it curried.
 // // Result for "Something" with n=4 should be "Some"
-var take = undefined;
+var take = slice(0)
+
+module.exports = {
+    words: words,
+    sentences: sentences,
+    filterQs: filterQs,
+    max: max,
+    slice: slice,
+    take: take
+};
